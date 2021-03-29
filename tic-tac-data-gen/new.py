8677130df7de140bb5aac2774ee2c76ad37f51e0
10sukhiay <73694548+10sukhiay@users.noreply.github.com>
2021-03-29 23:38:34 +0100

    board variable now working- redifine functions

--- tic-tac-data-gen/new.py
+++ tic-tac-data-gen/new.py
@@ -1,22 +1,11 @@
-import math
-import time
+player = 'O'
+bot = 'X'
 
-#define the board env elements with grid
-# Test for comment - B
-def printBoard(board):
-    print(board[1] + '|' + board[2] + '|' + board[3])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    print("\n")
-
-#func to check if space free
-def spaceIsFree(position):
-    if board[position] == ' ':
-        return True
-    else:
-        return False
+#func to allow user to input
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
 
 #func to insert x or o into free position
 def insertLetter(letter, position):
@@ -25,14 +14,14 @@ def insertLetter(letter, position):
         printBoard(board)
         if (checkDraw()):
             print("Draw!")
-            #exit()
+            exit()
         if checkForWin():
             if letter == 'X':
                 print("Bot wins!")
-                #exit()
+                exit()
             else:
                 print("Player wins!")
-                #exit()
+                exit()
         return
 
     else:
@@ -41,8 +30,22 @@ def insertLetter(letter, position):
         insertLetter(letter, position)
         return
 
+#func to check if space free
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+#check for a draw
+def checkDraw():
+    for key in board.keys():
+        if (board[key] == ' '):
+            return False
+    return True
+
 #func to check for win
-def checkForWin():
+def checkForWin(board):
     if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
         return True
     elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
@@ -62,104 +65,18 @@ def checkForWin():
     else:
         return False
 
-
-def checkWhichMarkWon(mark):
-    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
-        return True
-    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
-        return True
-    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
-        return True
-    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
-        return True
-    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
-        return True
-    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
-        return True
-    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
-        return True
-    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
-        return True
-    else:
-        return False
-
-#check for a draw
-def checkDraw():
-    for key in board.keys():
-        if (board[key] == ' '):
-            return False
-    return True
-
-#func to allow user to input
-def playerMove():
-    position = int(input("Enter the position for 'O':  "))
-    insertLetter(player, position)
-    return
-
-#comp move uses minimax algorithm
-def compMove():
-    bestScore = -800
-    bestMove = 0
-    time1 = time.time()
-    for key in board.keys():
-        if (board[key] == ' '):
-            board[key] = bot
-            score = minimax(board, 0, False)
-            board[key] = ' '
-            if (score > bestScore):
-                bestScore = score
-                bestMove = key
-
-    print(time.time() - time1)
-    insertLetter(bot, bestMove)
-    return
-
-#defines minimax algorithm
-def minimax(board, depth, isMaximizing):
-    if (checkWhichMarkWon(bot)):
-        return 1
-    elif (checkWhichMarkWon(player)):
-        return -1
-    elif (checkDraw()):
-        return 0
-
-    if (isMaximizing):
-        bestScore = - math.inf
-        for key in board.keys():
-            if (board[key] == ' '):
-                board[key] = bot
-                score = minimax(board, depth + 1, False)
-                board[key] = ' '
-                if (score > bestScore):
-                    bestScore = score
-        return bestScore
-
-    else:
-        bestScore = math.inf
-        for key in board.keys():
-            if (board[key] == ' '):
-                board[key] = player
-                score = minimax(board, depth + 1, True)
-                board[key] = ' '
-                if (score < bestScore):
-                    bestScore = score
-        return bestScore
-
 def initialise_board():
-    board = {1: ' ', 2: ' ', 3: ' ',
+    return ({1: ' ', 2: ' ', 3: ' ',
              4: ' ', 5: ' ', 6: ' ',
-             7: ' ', 8: ' ', 9: ' '}
-    return (board)
-
-start_time = time.time()
-board = {1: ' ', 2: ' ', 3: ' ',
-         4: ' ', 5: ' ', 6: ' ',
-         7: ' ', 8: ' ', 9: ' '}
+             7: ' ', 8: ' ', 9: ' '})
 
 def main():
-    while not checkForWin():
-        compMove()
+    board = initialise_board()
+    while not checkForWin(board):
         playerMove()
+
+    #print("Hello World!")
+
 
 if __name__ == "__main__":
     main()

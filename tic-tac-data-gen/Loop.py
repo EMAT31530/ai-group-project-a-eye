d73c8f0673a50a8c0177997ce2e18c2523e623f2
10sukhiay <73694548+10sukhiay@users.noreply.github.com>
2021-04-12 16:05:27 +0100

    mutiple changes

--- tic-tac-data-gen/Loop.py
+++ tic-tac-data-gen/Loop.py
@@ -1,8 +1,8 @@
-import new
+import Random_vs_Random
 import time
 
 start = time.time()
-for i in range(0, 5):
+for i in range(0, ):
     output = new.main()
     print(output)
 print('It took', time.time()-start, "seconds")
--- tic-tac-data-gen/Random_vs_Random.py
+++ tic-tac-data-gen/Random_vs_Random.py
@@ -5,65 +5,17 @@ import random
 player = 'O'
 bot = 'X'
 
-#func to allow user to input
-def playerMove(board):
-    position = int(input("Enter the position for 'O':  "))
-    insertLetter(player, position, board) #where player defines the letter played, bestMove defines the position played in, and board is a required input
-    return
-
 #func to allow playing a player who places pieces randomly
 def randMove(board):
     position = random.randint(1,9)
     insertLetterRand(player, position, board)
     return
 
-#comp move uses minimax algorithm- comp will always = bot
-def compMove(board):
-    bestScore = -800
-    bestMove = 0
-    #time1 = time.time()
-    for key in board.keys():
-        if (board[key] == ' '):
-            board[key] = bot
-            score = minimax(board, 0, False)
-            board[key] = ' '
-            if (score > bestScore):
-                bestScore = score
-                bestMove = key
-    #print(time.time() - time1)
-    insertLetter(bot, bestMove, board) #where bot defines the letter played, bestMove defines the position played in, and board is a required input
+#func to allow playing a player who places pieces randomly
+def randMove2(board):
+    position = random.randint(1,9)
+    insertLetterRand2(bot, position, board)
     return
-
-#defines minimax algorithm
-def minimax(board, depth, isMaximizing):
-    if (checkWhichMarkWon(bot, board)):
-        return 1
-    elif (checkWhichMarkWon(player, board)):
-        return -1
-    elif (checkDraw(board)): #does not need 'mark'- bot/player as an input as only checks if all spaces are full
-        return 0
-
-    if (isMaximizing):
-        bestScore = - math.inf
-        for key in board.keys():
-            if (board[key] == ' '):
-                board[key] = bot
-                score = minimax(board, depth + 1, False)
-                board[key] = ' '
-                if (score > bestScore):
-                    bestScore = score
-        return bestScore
-
-    else:
-        bestScore = math.inf
-        for key in board.keys():
-            if (board[key] == ' '):
-                board[key] = player
-                score = minimax(board, depth + 1, True)
-                board[key] = ' '
-                if (score < bestScore):
-                    bestScore = score
-        return bestScore
 
 def checkWhichMarkWon(mark, board):
     if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
@@ -95,43 +47,42 @@ def printBoard(board):
     print("\n")
 
 #func to insert x or o into free position
-def insertLetter(letter, position, board):
-    if spaceIsFree(position, board):
-        board[position] = letter
-        printBoard(board)
-        if (checkDraw(board)):
-            print("Draw!")
-            #exit()
-        if checkForWin(board):
-            if letter == 'X':
-                print("Bot wins!")
-                #exit()
-            else:
-                print("Bot loses!")
-                #exit()
-        return
-
-    else:
-        print("Can't insert there!")
-        position = int(input("Please enter new position:  "))
-        insertLetter(letter, position, board)
-        return
-
-#func to insert x or o into free position
 def insertLetterRand(letter, position, board):
     if spaceIsFree(position, board):
         board[position] = letter
         printBoard(board)
         if (checkDraw(board)):
             print("Draw!")
-            #exit()
+            exit()
         if checkForWin(board):
             if letter == 'X':
                 print("Bot wins!")
-                #exit()
+                exit()
             else:
                 print("Bot loses!")
-                #exit()
+                exit()
+        return
+
+    else:
+        position = random.randint(1,9)
+        insertLetterRand(letter, position, board)
+        return
+
+#func to insert x or o into free position
+def insertLetterRand2(letter, position, board):
+    if spaceIsFree(position, board):
+        board[position] = letter
+        printBoard(board)
+        if (checkDraw(board)):
+            print("Draw!")
+            exit()
+        if checkForWin(board):
+            if letter == 'X':
+                print("Bot wins!")
+                exit()
+            else:
+                print("Bot loses!")
+                exit()
         return
 
     else:
@@ -183,10 +134,10 @@ def main():
     board = initialise_board()
     while not checkForWin(board):
         #print('checkforwinnotcompleted')
-        compMove(board)
+        randMove2(board)
         if not checkForWin(board):
             #print('checkforwinnotcompleted')
-            playerMove(board)
+            randMove(board)
 
 if __name__ == "__main__":
     main()

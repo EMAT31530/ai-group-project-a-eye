4d060a047bf45ef99d8a9d9a1c173226f787f269
Woos100 <73696605+Woos100@users.noreply.github.com>
2021-04-14 12:25:05 +0100

    initial commit

---
+++ tic-tac-data-gen/on_repeat.py
@@ -0,0 +1,15 @@
+import experiment_with_compMove2
+import numpy
+
+#it = 2
+#saved_games = numpy.zeros(it)
+#saved_games = [1,1]
+#print(saved_games)
+
+#for i in range(len(saved_games)):
+    #saved_games[i] = experiment_with_compMove2.main()
+
+
+#print(saved_games)
+
+experiment_with_compMove2.py
